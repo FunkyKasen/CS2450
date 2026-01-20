e19416a7a91ad73618c50e6fda953ee268d1b8b4
FunkyKasen <kasenrayce2@gmail.com>
2026-01-20 10:03:46 -0700

    first commit

---
+++ age_guesser.py
@@ -0,0 +1,17 @@
+import random
+
+print("Hello! The goal for this app is to try and guess your age.")
+
+name = input("What's your name? ")
+
+ages = list(range(15, 31))
+random.shuffle(ages)
+    
+for age in ages:
+    answer = input(f"Are you {age} years old? (y/n): ").lower()
+
+    if answer == "y":
+        print(f"Yes! {name} is {age} years old.")
+        break
+    else:
+        print("Rats.")
